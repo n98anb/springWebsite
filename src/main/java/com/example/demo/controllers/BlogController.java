844fec347e62370c92e275004af67ffa6cc2ee2c
Vsevolod Bochkarev <bochkarevvv777@mail.ru>
2024-09-30 00:09:57 +0300

    second commit

--- src/main/java/com/example/demo/controllers/BlogController.java
+++ src/main/java/com/example/demo/controllers/BlogController.java
@@ -1,13 +1,21 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Post;
+import com.example.demo.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 
 @Controller
 public class BlogController {
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping("/blog")
         public String blogMain(Model model){
+            Iterable<Post> posts = postRepository.findAll();
+            model.addAttribute("posts", posts);
             return "blog-main";
         }
 }
